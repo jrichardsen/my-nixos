95dfccffcf66b6a3d97d7cda9f3cdb55aa24e6bb
jrichardsen <jonas.richardsen@gmail.com>
2025-09-28 21:13:09 +0200

    configure librewolf default profile
    Signed-off-by: jrichardsen <jonas.richardsen@gmail.com>

--- homeManagerModules/features/presets/librewolf.nix
+++ homeManagerModules/features/presets/librewolf.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   cfg = config.features.presets.librewolf;
 in
@@ -11,7 +16,73 @@ with lib;
   };
 
   config = mkIf cfg.enable {
-    stylix.targets.librewolf.profileNames = [ ];
+
+    programs.librewolf = {
+      profiles = {
+        default = {
+          isDefault = true;
+          search = {
+            force = true;
+            engines = {
+              nix-packages = {
+                name = "Nix Packages";
+                urls = [ { template = "https://search.nixos.org/packages?query={searchTerms}"; } ];
+
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [
+                  "@nixpkgs"
+                  "@np"
+                ];
+              };
+              nixos-options = {
+                name = "NixOS Options";
+                urls = [ { template = "https://search.nixos.org/options?query={searchTerms}"; } ];
+
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [
+                  "@nixopts"
+                  "@no"
+                ];
+              };
+              home-manager-options = {
+                name = "HomeManager Options";
+                urls = [ { template = "https://home-manager-options.extranix.com/?query={searchTerms}"; } ];
+
+                iconMapObj."16" = "https://home-manager-options.extranix.com/images/favicon.png";
+                definedAliases = [
+                  "@hmopts"
+                  "@hm"
+                ];
+              };
+              nixos-wiki = {
+                name = "NixOS Wiki";
+                urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
+                iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+                definedAliases = [
+                  "@nixwiki"
+                  "@nw"
+                ];
+              };
+            };
+          };
+          settings = {
+            # Preserve browsing and download history
+            "privacy.clearOnShutdown.history" = false;
+            "privacy.clearOnShutdown.downloads" = false;
+            "privacy.clearOnShutdown_v2.browsingHistoryAndDownloads" = false;
+
+            "sidebar.verticalTabs" = true;
+          };
+          extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+            darkreader
+            vimium
+            bitwarden
+          ];
+        };
+      };
+    };
+
+    stylix.targets.librewolf.profileNames = [ "default" ];
     systemInterface.applications.webBrowser = mkIf config.programs.librewolf.enable "librewolf";
   };
 }
